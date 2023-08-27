b7196d41f08a471eb6e59dca55886bcc4e093fd0
Divy Solanki <divysolanki7@gmail.com>
2023-08-27 14:18:12 +0530

    Fixed EDA file and model training file

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+import logger
 
 def error_message_details(error, error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -17,4 +18,3 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
-    
